f2a7b70fe368cb13814485ab3b7b7ea8d00b1bd2
SiddhuPudi <thrivikram.pudi00@gmail.com>
2026-04-03 05:23:34 +0530

    refactor: Updated Swagger server URL

--- Backend/src/config/swagger.js
+++ Backend/src/config/swagger.js
@@ -9,7 +9,7 @@ const options = {
             description: "API documentation for Talent Sync backend"
         },
         servers: [
-            { url: "https://talent-sync-pq7j.onrender.com" }
+            { url: process.env.NODE_ENV === "production" ? "https://talent-sync-pq7j.onrender.com" : "http://localhost:3000" }
         ],
         components: {
             securitySchemes: {
